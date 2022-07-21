a4621ff9b508f16b2950af591881fa15f2273350
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-07-22 00:18:32 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import discord
+from discord.ext import tasks, commands
+import datetime
+import asyncio
+from flask import Flask
+from threading import Thread
+
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix='a!')
+bot.remove_command('help')
+suimin = 949560203886604293
+token = os.environ['token']
+@bot.event
+async def on_ready():
+    print('ready')
+    channel = bot.get_channel(949560203886604293)
+    channel2 = bot.get_channel(984807772950519890)
+    print(channel)
+    print(channel2)
+    ziholoop.start()
+    await bot.change_presence(activity=discord.Game(name=f"0時まで待機中"))
+
+@tasks.loop(seconds=1)
+async def ziholoop():
+   dt_now = datetime.datetime.now()
+   if int(dt_now.hour) == 0 and int(dt_now.minute) == 00:
+        channel = bot.get_channel(949560203886604293)
+        channel2 = bot.get_channel(984807772950519890)
+        print(channel)
+        await channel.send(f'いちこめ {dt_now.month}/{dt_now.date}です')
+        await channel2.send(f'いちこめ {dt_now.month}/{dt_now.date}です')
+        await bot.change_presence(activity=discord.Game(name=f"0時過ぎたぞ"))
+        await asyncio.sleep(60)
+bot.run(token)
